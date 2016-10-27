f9c374efd2bd4064ee0e2219af119b360737a116
jens buß <jens.buss@tu-dortmund.de>
2016-10-27 18:55:49 +0200

    add imports for subpackage ClassifierCharacteristics and preparation

--- disteval/scripts/__init__.py
+++ disteval/scripts/__init__.py
@@ -1,1 +1,5 @@
-#put executables here
+# -*- coding:utf-8 -*-
+from __future__ import absolute_import, print_function, division
+
+from .classifier_characteristics import ClassifierCharacteristics
+from .preparation import prepare_data, shrink_data, convert_and_remove_non_finites
