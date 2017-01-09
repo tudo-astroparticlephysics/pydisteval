0554e04b08b5bb16f07c80c32eb804f3ab03025f
Mathis Börner <mathis.boerner@tu-dortmund.de>
2017-01-09 18:49:22 +0100

    extended logging

--- disteval/visualization/comparison_plotter/__init__.py
+++ disteval/visualization/comparison_plotter/__init__.py
@@ -1,4 +1,5 @@
 from inspect import isclass
+import logging
 
 from matplotlib import pyplot as plt
 from matplotlib.gridspec import GridSpec
@@ -11,6 +12,8 @@ REGISTERED_ELEMENTS = {'aggarwalhisto': elements.AggarwalHisto,
                        'aggarwalratio': elements.AggarwalHisto,
                        'classichisto': elements.ClassicHisto,
                        'classicratio': elements.ClassicRatio}
+
+logger = logging.getLogger("Plotter")
 
 class ComparisonPlotter:
     def __init__(self, title=None, n_bins=50):
@@ -29,17 +32,27 @@ class ComparisonPlotter:
             pass
         else:
             raise TypeError('Invalid Type \'element\'!')
+        logger.info('Adding {}! (Element)'.format(element.name))
         element.register(self)
 
     def register_calc_part(self, part):
         if not part in self.calc_parts:
+            logger.info('\tRegistered {} (CalcPart)!'.format(part.name))
             self.calc_parts.append(part)
 
     def register_plot_part(self, part):
         if not part in self.calc_parts:
+            logger.info('\tRegistered {} (PlotPart)!'.format(part.name))
             self.plot_parts.append(part)
 
-    def add_ref(self, label, X, weights=None, color=None, cmap=None):
+    def add_ref(self,
+                label,
+                X,
+                livetime=1,
+                weights=None,
+                color=None,
+                cmap=None):
+        logger.info('Added \'{}\' (Ref-Component)!'.format(label))
         self.components.append(Component(idx=len(self.components),
                                          label=label,
                                          c_type='ref',
@@ -48,7 +61,13 @@ class ComparisonPlotter:
                                          color=color,
                                          cmap=cmap))
 
-    def add_ref_part(self, label, X, weights=None, color=None):
+    def add_ref_part(self,
+                     label,
+                     X,
+                     livetime=1,
+                     weights=None,
+                     color=None):
+        logger.info('Added \'{}\' (RefPart-Component)!'.format(label))
         self.components.append(Component(idx=len(self.components),
                                          label=label,
                                          c_type='ref_part',
@@ -56,7 +75,13 @@ class ComparisonPlotter:
                                          weights=weights,
                                          color=color))
 
-    def add_test(self, label, X, weights=None, color=None):
+    def add_test(self,
+                 label,
+                 X,
+                 livetime=1,
+                 weights=None,
+                 color=None):
+        logger.info('Added \'{}\' (Test-Component)!'.format(label))
         self.components.append(Component(idx=len(self.components),
                                          label=label,
                                          c_type='test_part',
@@ -64,7 +89,13 @@ class ComparisonPlotter:
                                          weights=weights,
                                          color=color))
 
-    def add_test_part(self, label, X, weights=None, color=None):
+    def add_test_part(self,
+                      label,
+                      X,
+                      livetime=1,
+                      weights=None,
+                      color=None):
+        logger.info('Added \'{}\' (TestPart-Component)!'.format(label))
         self.components.append(Component(idx=len(self.components),
                                          label=label,
                                          c_type='test_part',
@@ -73,21 +104,26 @@ class ComparisonPlotter:
                                          color=color))
 
     def draw(self, x_label='Feature', fig=None, figsize=(10, 8)):
+        logger.info('Start Draw Process!')
+        logger.info('===================')
         result_tray = self.calc()
         if not isinstance(fig, plt.Figure):
             self.last_fig = plt.figure(figsize=figsize)
         total_rows = sum([part_i.rows for part_i in self.plot_parts])
         gs = GridSpec(nrows=total_rows, ncols=1)
         row_point = 0
+        logger.info('Starting Plotting...')
         for part_i in self.plot_parts:
             row_slice = slice(row_point, row_pointer + part_i.get_height())
             col_slice = slice(None)
             part_i.set_ax(fig, gs[row_slice, col_slice])
             for comp_i in self.components:
                 result_tray = part_i.execute(result_tray, comp_i)
+        logger.info('Finished!')
         return fig
 
     def calc(self):
+        logger.info('Starting Calculating...')
         result_tray = ResultTray()
         sorted(self.calc_parts)
         sorted(self.components)
@@ -95,6 +131,7 @@ class ComparisonPlotter:
             for comp_i in self.components:
                 result_tray = part_i.execute(result_tray, comp_i)
             part_i.reset()
+        logger.info('Finished!')
         return result_tray
 
     def reset(self):
