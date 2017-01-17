4c1a02b331ad23254d6ace688b795fa117abcc7c
Mathis Börner <mathis.boerner@tu-dortmund.de>
2017-01-17 16:04:38 +0100

    fixed legend aggarwal and finish

--- tests/test_comp_plotter.py
+++ tests/test_comp_plotter.py
@@ -10,17 +10,22 @@ logging.basicConfig(
         level=logging.DEBUG)
 
 plotter = ComparisonPlotter()
-#plotter.add_element('ClassicHisto', bands=False)
-#plotter.add_element('ClassicRatio', bands=False, y_lims=[-5, 5])
+
+
 plotter.add_element('AggarwalHisto')
 plotter.add_element('AggarwalRatio')
 plotter.add_element('Normalization', normalize='test_livetime')
-X_ref = np.random.normal(loc=0., size=100000)
-X_test = np.random.normal(loc=0, size=1000)
-plotter.add_ref('Reference', X_ref, livetime=100, color='w')
-#plotter.add_ref_part('Reference Part 2', X_ref[:5000], livetime=100)
-#plotter.add_ref_part('Reference Part 1', X_ref[5000:15000], livetime=100)
-plotter.add_test('Test', X_test, livetime=1)
-#plotter.add_test_part('Test Part 2', X_test[:500], livetime=1)
-#plotter.add_test_part('Test Part 1', X_test[500:], livetime=1)
-plotter.draw()
+
+
+
+for i in range(10):
+    X_ref = np.random.normal(loc=0., size=100000)
+    X_test = np.random.normal(loc=0, size=1000)
+    plotter.add_ref('Sum MC', X_ref, livetime=100, color='k')
+    plotter.add_ref_part('Corsika', X_ref[:3000], livetime=100)
+    plotter.add_ref_part('Corsika', X_ref[3000:4000], livetime=100)
+    plotter.add_test('Data', X_test, livetime=1)
+
+    fig, ax_dict, _ = plotter.draw()
+    fig.savefig('test{}.png'.format(i))
+    plotter.finish()
