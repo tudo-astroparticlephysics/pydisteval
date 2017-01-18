603ca4bb4732cb2b8a102b1a926e976185fba3f5
Mathis Börner <mathis.boerner@tu-dortmund.de>
2017-01-18 11:13:42 +0100

    added binning_dict funtionality

--- disteval/visualization/comparison_plotter/elements.py
+++ disteval/visualization/comparison_plotter/elements.py
@@ -2,41 +2,18 @@ from .base_classes import Element
 from . import parts
 
 
-class AggarwalHisto(Element):
-    name = 'AggarwalHisto'
-
-    def __init__(self,
-                 n_bins=50,
-                 log_y=True,
-                 alpha=[0.68, 0.9, 0.95],
-                 bands=False,
-                 band_borders=True,
-                 band_brighten=True,
-                 band_alpha=0.5,
-                 y_label='Frequence'):
-        self.calc_components.append(parts.CalcBinning(n_bins=n_bins))
-        self.calc_components.append(parts.CalcHistogram())
-        self.calc_components.append(parts.CalcAggarwalHistoErrors(alpha))
-        plot_hist = parts.PlotHistAggerwal(log_y=log_y,
-                                           bands=bands,
-                                           band_borders=band_borders,
-                                           band_brighten=band_brighten,
-                                           band_alpha=band_alpha,
-                                           y_label=y_label)
-        self.plot_components.append(plot_hist)
-
-
 class ClassicHisto(Element):
     name = 'ClassicHisto'
 
     def __init__(self,
                  n_bins=50,
+                 y_label='Frequence',
                  log_y=True,
                  bands=False,
                  band_borders=True,
                  band_brighten=True,
                  band_alpha=0.5,
-                 y_label='Frequence'):
+                 binning_dict=None):
         self.calc_components.append(parts.CalcBinning(n_bins=n_bins))
         self.calc_components.append(parts.CalcHistogram())
         self.calc_components.append(parts.CalcClassicHistoErrors())
@@ -49,34 +26,21 @@ class ClassicHisto(Element):
         self.plot_components.append(plot_hist)
 
 
-class AggarwalRatio(Element):
-    name = 'AggarwalRatio'
-
-    def __init__(self,
-                 n_bins=50,
-                 alpha=[0.68, 0.9, 0.95],
-                 zoomed=True,
-                 y_label='p-value*'):
-        self.calc_components.append(parts.CalcBinning(n_bins=n_bins))
-        self.calc_components.append(parts.CalcHistogram())
-        self.calc_components.append(parts.CalcAggarwalHistoErrors(alpha))
-        self.calc_components.append(parts.CalcAggarwalRatios())
-        self.plot_components.append(parts.PlotRatioAggerwal(zoomed=zoomed,
-                                                            y_label=y_label))
-
-
 class ClassicRatio(Element):
     name = 'ClassicRatio'
 
     def __init__(self,
                  n_bins=50,
+                 y_label=r'$\frac{\mathregular{Test - Ref}}{\sigma}$',
                  bands=False,
                  band_borders=True,
                  band_brighten=True,
                  band_alpha=0.5,
                  y_lims=None,
-                 y_label=r'$\frac{\mathregular{Test - Ref}}{\sigma}$'):
-        self.calc_components.append(parts.CalcBinning(n_bins=n_bins))
+                 binning_dict=None):
+        self.calc_components.append(parts.CalcBinning(
+            n_bins=n_bins,
+            binning_dict=binning_dict))
         self.calc_components.append(parts.CalcHistogram())
         self.calc_components.append(parts.CalcClassicHistoErrors())
         plot_ratio = parts.PlotRatioClassic(bands=bands,
@@ -86,6 +50,52 @@ class ClassicRatio(Element):
                                             y_lims=y_lims,
                                             y_label=y_label)
         self.plot_components.append(plot_ratio)
+
+
+class AggarwalHisto(Element):
+    name = 'AggarwalHisto'
+
+    def __init__(self,
+                 n_bins=50,
+                 y_label='Frequence',
+                 log_y=True,
+                 alpha=[0.68, 0.9, 0.95],
+                 bands=False,
+                 band_borders=True,
+                 band_brighten=True,
+                 band_alpha=0.5,,
+                 binning_dict=None):
+        self.calc_components.append(parts.CalcBinning(
+            n_bins=n_bins,
+            binning_dict=binning_dict))
+        self.calc_components.append(parts.CalcHistogram())
+        self.calc_components.append(parts.CalcAggarwalHistoErrors(alpha))
+        plot_hist = parts.PlotHistAggerwal(log_y=log_y,
+                                           bands=bands,
+                                           band_borders=band_borders,
+                                           band_brighten=band_brighten,
+                                           band_alpha=band_alpha,
+                                           y_label=y_label)
+        self.plot_components.append(plot_hist)
+
+
+class AggarwalRatio(Element):
+    name = 'AggarwalRatio'
+
+    def __init__(self,
+                 n_bins=50,
+                 y_label='p-value*',
+                 alpha=[0.68, 0.9, 0.95],
+                 zoomed=True,
+                 binning_dict=None):
+        self.calc_components.append(parts.CalcBinning(
+            n_bins=n_bins,
+            binning_dict=binning_dict))
+        self.calc_components.append(parts.CalcHistogram())
+        self.calc_components.append(parts.CalcAggarwalHistoErrors(alpha))
+        self.calc_components.append(parts.CalcAggarwalRatios())
+        self.plot_components.append(parts.PlotRatioAggerwal(zoomed=zoomed,
+                                                            y_label=y_label))
 
 
 class Normalization(Element):
