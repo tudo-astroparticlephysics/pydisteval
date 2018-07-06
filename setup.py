1e3ef99d50f0c509b0b5ea3396f7dd269d42caaf
Maximilian Meier <mmeier@vollmond.app.tu-dortmund.de>
2018-07-06 14:49:24 +0200

    Travis

--- setup.py
+++ setup.py
@@ -56,5 +56,5 @@ setup(
         'console_scripts': ['fact_example=examples.fact_example.py:main'],
     },
     setup_requires=['pytest-runner'],
-    tests_require=['pytest>=3.1.0', 'setuptools'],
+    tests_require=['pytest>=3.1.0', 'setuptools>=34.4.1'],
 )
